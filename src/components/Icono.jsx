2f4b0415f4a145cec7162f4ac29f54a75d645b07
Fernando <fernandoesc180@gmail.com>
2026-05-08 14:05:07 -0600

    Corrige alineacion e iconos finales

--- src/components/Icono.jsx
+++ src/components/Icono.jsx
@@ -1,3 +1,20 @@
+const iconosSvg = {
+  logout: (
+    <svg viewBox="0 0 24 24" focusable="false">
+      <path d="M10 6H6v12h4" />
+      <path d="M14 8l4 4-4 4" />
+      <path d="M18 12H9" />
+    </svg>
+  ),
+  'cloud-saving': (
+    <svg viewBox="0 0 24 24" focusable="false">
+      <path d="M7 18H6.5a4 4 0 0 1-.6-7.9A6 6 0 0 1 17 8.6 4.7 4.7 0 0 1 18 18h-1" />
+      <path d="M12 12v7" />
+      <path d="M8.8 15.2 12 12l3.2 3.2" />
+    </svg>
+  ),
+};
+
 /**
  * Representa un icono usando clases CSS segun su nombre.
  *
@@ -6,7 +23,11 @@
  * @returns {JSX.Element} Elemento visual del icono.
  */
 function Icono({ nombre }) {
-  return <span className={`icono icono--${nombre}`} aria-hidden="true" data-figma-name="Icono" />;
+  return (
+    <span className={`icono icono--${nombre}`} aria-hidden="true" data-figma-name="Icono">
+      {iconosSvg[nombre]}
+    </span>
+  );
 }
 
 export default Icono;
